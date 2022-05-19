9f799b20f846adf49e83c3fbc12e97f2bfd047f9
tchen <tchen@murex.com>
2022-05-19 19:15:29 +0200

    add tdd test code

--- core-tdd-test-pyramid/src/main/java/tdd/Calculator.java
+++ core-tdd-test-pyramid/src/main/java/tdd/Calculator.java
@@ -3,6 +3,15 @@ package tdd;
 import java.util.Arrays;
 import java.util.OptionalDouble;
 
+// TDD 测试原则: 使用测试来驱动生产代码的编写
+// Test Driven Development
+// 1. Write the test cases before codes
+// 2. Use the test cases to drive the design of code
+// 3. Refactor the code after testing
+
+// 1. Create a unit tests that fails
+// 2. Write production code that makes that test pass.
+// 3. Clean up the mess you just made.
 public class Calculator {
 
     public int add(int value1, int value2) {
@@ -14,5 +23,19 @@ public class Calculator {
         // if the array is empty, .average() will return an empty optional
         OptionalDouble average = Arrays.stream(array).average();
         return average.isPresent() ? average.getAsDouble() : 0;
+    }
+
+    // For debug testing demo
+    // For test coverage demo: hit the conditions 需要击中每一个分支的条件
+    public String findScoreRange(int score) throws Exception {
+        if (score >= 0 && score < 60) {
+            return "failed";
+        } else if (score >= 60 && score < 80) {
+            return "good";
+        } else if (score >= 80 && score <= 100) {
+            return "great";
+        } else {
+            throw new Exception("Can not find the range of score");
+        }
     }
 }
--- core-tdd-test-pyramid/src/test/java/tdd/TddTest.java
+++ core-tdd-test-pyramid/src/test/java/tdd/TddTest.java
@@ -23,4 +23,19 @@ public class TddTest {
         double average = calculator.average(array);
         Assert.assertEquals(4, average, 0.0);
     }
+
+    @Test
+    public void test_find_score_range() throws Exception {
+        Calculator calculator = new Calculator();
+        String result = calculator.findScoreRange(65);
+        Assert.assertEquals("good", result);
+    }
+
+    @Test
+    public void test_find_score_range_with_exception() throws Exception {
+        Calculator calculator = new Calculator();
+        // We can pass a lambda expression as implementation of the interface
+        // We check and make sure it will throw this type of exception.
+        Assert.assertThrows(Exception.class, () -> calculator.findScoreRange(200));
+    }
 }
