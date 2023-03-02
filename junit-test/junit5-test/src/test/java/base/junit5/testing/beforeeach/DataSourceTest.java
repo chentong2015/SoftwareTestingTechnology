c148095c07e78252779da618c588c08c23030edb
tchen <tchen@murex.com>
2023-03-02 14:38:54 +0100

    refresh junit5 test

--- junit-test/junit5-test/src/test/java/base/junit5/testing/beforeeach/DataSourceTest.java
+++ junit-test/junit5-test/src/test/java/base/junit5/testing/beforeeach/DataSourceTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 class DataSourceTest extends AbstractSourceTest {
 
     // TODO. 重写父类的@BeforeEach方法将导致其不再具有before each的特性
-    // 1. @Override 会导致只有子类方法被执行
-    // 2. 如果父类和子类都有纯BeforeEach方法，则先后都会执行
+    // 1. @Override重写父类的@BeforeEach方法会导致不会执行
+    // 2. 如果父类和子类都有各自的BeforeEach方法(方法签名不同)，则先后都会执行
     @BeforeEach
-    public void setUp2() {
+    public void setUp() {
+        // 3. super.setUp(); 方法签名相同，则可以通过super调用父类的setUp()方法
         System.out.println("Override setup method");
     }
 
--- junit-test/junit5-test/src/test/java/com/common/api/JunitCommonClassTest.java
+++ junit-test/junit5-test/src/test/java/com/common/api/JunitCommonClassTest.java
@@ -3,12 +3,13 @@ package com.common.api;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
+import java.util.List;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // 如果测试类和原始类在同一个路径下，则不需要引入package
 class JunitCommonClassTest {
@@ -21,12 +22,18 @@ class JunitCommonClassTest {
         Assertions.assertTrue(true);
     }
 
-    // 使用test-api中classpath下的文件
+    // 使用test-api中classpath下的文件: 优先选择当前项目classpath下的CSV文件
     @Test
-    void testClasspathResources() {
+    void testClasspathResources() throws IOException {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = classloader.getResourceAsStream("sample.csv");
         Assertions.assertNotNull(inputStream);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            Stream<String> streams = bufferedReader.lines().skip(1);
+            List<String> lines = streams.collect(Collectors.toList());
+            System.out.println(lines.get(0));
+            System.out.println(lines.get(1));
+        }
     }
 
     // 拿到connection.properties文件中的配置信息
